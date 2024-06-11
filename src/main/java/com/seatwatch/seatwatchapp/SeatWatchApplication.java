dd1491eb75853087d2a9a58779b3df05f8bdfaeb
Ross Learned <rclearned1@hotmail.com>
2024-06-10 22:19:33 -0700

    Initial commit

---
+++ src/main/java/com/seatwatch/seatwatchapp/SeatWatchApplication.java
@@ -0,0 +1,13 @@
+package com.seatwatch.seatwatchapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeatWatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeatWatchApplication.class, args);
+	}
+
+}
