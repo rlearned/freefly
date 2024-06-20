b6feddd14ba35174472e5608c1349e36cb124995
Ross Learned <rclearned1@hotmail.com>
2024-06-19 22:45:09 -0700

    Added Route repository

---
+++ src/main/java/com/freefly/freeflyapp/repository/RouteRepository.java
@@ -0,0 +1,7 @@
+package com.freefly.freeflyapp.repository;
+
+import com.freefly.freeflyapp.model.Route;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RouteRepository extends JpaRepository<Route, Long> {
+}
