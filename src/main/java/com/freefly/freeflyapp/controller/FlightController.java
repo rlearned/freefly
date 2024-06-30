83031a5dd857a692de4ec0a28d17b03402b99e17
rlearned <rclearned1@hotmail.com>
2024-06-29 21:58:17 -0700

    Added autowiring for flight service and request mapping for the flightController

--- src/main/java/com/freefly/freeflyapp/controller/FlightController.java
+++ src/main/java/com/freefly/freeflyapp/controller/FlightController.java
@@ -1,4 +1,15 @@
 package com.freefly.freeflyapp.controller;
 
+import com.freefly.freeflyapp.service.FlightService;
+import com.freefly.freeflyapp.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/flights")
 public class FlightController {
+
+    @Autowired
+    private FlightService flightService;
 }
