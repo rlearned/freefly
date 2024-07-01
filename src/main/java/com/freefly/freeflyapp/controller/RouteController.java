30d26938747363578d02c85e8274a4037cb6ed6b
rlearned <rclearned1@hotmail.com>
2024-06-30 22:57:16 -0700

    Added request mapping for route controller

--- src/main/java/com/freefly/freeflyapp/controller/RouteController.java
+++ src/main/java/com/freefly/freeflyapp/controller/RouteController.java
@@ -1,2 +1,15 @@
-package com.freefly.freeflyapp.controller;public class RouteController {
+package com.freefly.freeflyapp.controller;
+
+import com.freefly.freeflyapp.service.RouteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/routes")
+public class RouteController {
+
+    @Autowired
+    private RouteService routeService;
+
 }
